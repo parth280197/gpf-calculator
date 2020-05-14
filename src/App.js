a4ebcc3295e6b59a5a2cf596c3a893a1273a87f5
Parth <parth280197@gmail.com>
2020-05-13 21:49:34 -0500

    Datepicker added with mobx and other utils

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import {action, observable} from "mobx";
-import { observer } from "mobx-react"
 import './App.css';
+import "react-datepicker/dist/react-datepicker.css";
+import DateSelector from "./Components/DateSelector/DateSelector";
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 
-@observer
 class App extends React.Component {
-    @observable price = 0;
-    @action
-    onClickHandler = () => this.price++;
     render() {
     return (
         <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                {this.price} <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                onClick={this.onClickHandler}
-                className="App-link"
-                // href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <DateSelector/>
+            </MuiPickersUtilsProvider>
         </div>
     );
   }
