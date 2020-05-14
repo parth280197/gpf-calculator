59062b7e39ee3c71494103852eedc4db3ce22b86
Parth <parth280197@gmail.com>
2020-05-14 13:38:04 -0500

    Updates

---
+++ precache-manifest.da46977532a7356a043acf962e29efb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c63624b3acd94d652cc3d5e11d3fd1",
+    "url": "/gpf-calculator/index.html"
+  },
+  {
+    "revision": "762a675c31b7af598583",
+    "url": "/gpf-calculator/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "a31cbccc2b5ec2137e1e",
+    "url": "/gpf-calculator/static/css/main.7dd38413.chunk.css"
+  },
+  {
+    "revision": "762a675c31b7af598583",
+    "url": "/gpf-calculator/static/js/2.055d21ed.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/gpf-calculator/static/js/2.055d21ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31cbccc2b5ec2137e1e",
+    "url": "/gpf-calculator/static/js/main.fb642090.chunk.js"
+  },
+  {
+    "revision": "c2a965b75ee7301318af",
+    "url": "/gpf-calculator/static/js/runtime-main.39568743.js"
+  }
+]);
